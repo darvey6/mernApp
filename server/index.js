0444726adf3f3a1b0cb6d7bd3051ff9f192a40b2
Darvey <darvey6@hotmail.com>
2021-07-27 17:10:47 -0700

    test

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
 })
 
-
+//
     // app.use(express.static(path.join(__dirname, '../build')));
     // app.get('*', function(req, res) {
     //     res.sendFile(path.join(__dirname, '../build', 'index.html'));
